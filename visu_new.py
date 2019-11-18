bb09fa3dffa96af2f2cafc589d6b247d2130cb50
Thomas K Ales [MSE] <tkales@iastate.edu>
2019-11-18 08:42:57 -0600

    multi-tab working

--- visu_new.py
+++ visu_new.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMessageBox, QTa
 import glob
 import os
 import sys
-
+from functools import partial
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
@@ -34,7 +34,7 @@ class Ui(QMainWindow):
         self.system_figure_axes = [self.system_figures[0].add_subplot("111")]
         self.system_figure_axes[0].plot(np.random.rand(5))
         self.addmpl(0, self.system_figures[0])
-        
+        self.connectAngleTab(0)
         # figure_1 = Figure()
         # fig1_axes1 = figure_1.add_subplot("111")
         # fig1_axes1.plot(np.random.rand(5))
@@ -57,8 +57,28 @@ class Ui(QMainWindow):
             self.system_figure_axes.append(self.system_figures[-1].add_subplot("111"))
             self.system_figure_axes[-1].plot(np.random.rand(5))
             self.addmpl((self.system_figures.__len__()-1), self.system_figures[-1])
-        
+            self.connectAngleTab(tab_count)
+
+    def connectAngleTab(self, tab_index):
+        """
+            connectAngleTab: Wires up all the signals and slots for the various 
+                             UI elements in each tab. Only really designed to be
+                             called from addAngleTab
+        """   
+        self.current_tabs[tab_index].select_scandir_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].process_scope_data_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].load_saved_data_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].plot_dc_intensity_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].plot_sample_mask_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].plot_fft_map_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].patch_spacing_textedit.textChanged.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].plot_velocity_map_button.clicked.connect(partial(self.noFunctionalityDialog, tab_index))
+        self.current_tabs[tab_index].saw_grating_angle_lineedit.textChanged.connect(partial(self.noFunctionalityDialog, tab_index))
+
     def addmpl(self, tab_index, fig):
+        """
+        addmpl takes a tab index and figure and then plots that information.
+        """
         self.canvas = FigureCanvas(fig)
         self.current_tabs[tab_index].mpl_vlayout.addWidget(self.canvas)
         self.canvas.draw()
@@ -71,6 +91,13 @@ class Ui(QMainWindow):
         self.current_tabs[tab_index].mpl_vlayout.removeWidget(self.navbar)
         self.navbar.close()
 
+    def noFunctionalityDialog(self, tab_number):
+        oops_dialog = QMessageBox()
+        print("This functionality has not been implemented yet.")
+        oops_dialog.setText("This functionality has not been implemented yet.\n" +
+        "Signal Received from tab #{0}".format(tab_number))
+        oops_dialog.exec()
+        return
 app = QApplication(sys.argv)
 window = Ui()
 app.exec_()
