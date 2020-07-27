97c960e997280410f77c434984f5f8f7184698ff
Thomas Ales <alestk@gmail.com>
2020-07-27 09:32:19 -0500

    synch

--- MapData.py
+++ MapData.py
@@ -2,19 +2,21 @@
     MapData Objects
     Thomas Ales
     June 2020
+
+    CHANGELOG:
+    v0.0 - Initial Release.
+    v0.1 - Cleaned up code to conform to FLAKE8.
+           Removed vestigal functions/properties.
+           Removed unnecessary dependencies.
 '''
-import gc
 import glob
+from multiprocessing import Pool
 import os
 
-import matplotlib.pyplot as plt
 import pandas as pd
 
 import TekData
-import pyfftw
-import tekwfm
 
-from multiprocessing import Pool
 
 class TekMap:
 
@@ -27,17 +29,17 @@ class TekMap:
 
     map_directory = ""
     map_aspect = 0
-    
+
     waveforms = []
 
     rf_voltagemap = []
 
-    
     def __init__(self, _datadir):
         '''
-        TekMap(_datadir): Constructor, requires _datadir which is an absolute path to the WFM directory.
+        TekMap(_datadir): Constructor, requires _datadir which is an absolute
+        path to the WFM directory.
         '''
-        
+
         self.map_directory = _datadir
         self.rf_filelist = []
         self.dc_filelist = []
@@ -46,45 +48,47 @@ class TekMap:
         self.fft_map = []
         self.velocity_map = []
         self.waveforms = []
-        self.thresholddc = 0.100            # Starting mask voltage 
-        self.metadata = {"acq_angle": 0, 
-                "x_start": 0, 
-                "y_start": 0, 
-                "x_delta": 0, 
-                "y_delta": 0,
-                "sample_desc": ""}
-        # Search for files in the map_directory. Sort into RF and DC waveforms
+        self.thresholddc = 0.100
+        self.metadata = {"acq_angle": 0,
+                         "x_start": 0,
+                         "y_start": 0,
+                         "x_delta": 0,
+                         "y_delta": 0,
+                         "sample_desc": ""}
+
+        # Search for files in the map_directory.
+        # Sort into RF and DC waveforms
         os.chdir(self.map_directory)
         for current_file in glob.glob("*.wfm"):
             if(current_file.startswith("RF")):
-                self.rf_filelist.append(self.map_directory + "/" + current_file)
+                self.rf_filelist.append(self.map_directory +
+                                        "/" + current_file)
 
             elif(current_file.startswith("DC")):
-                self.dc_filelist.append(self.map_directory + "/" + current_file)
+                self.dc_filelist.append(self.map_directory +
+                                        "/" + current_file)
 
         self.rf_filelist.sort()
         self.dc_filelist.sort()
-        print("Found {0} & {1} DC / RF Files".format(self.rf_filelist.__len__(),
-                                                      self.dc_filelist.__len__()))
+        self.tekwfmclass = TekData.TekWaveForm(self.rf_filelist[0],
+                                               self.dc_filelist[0])
 
-        self.tekwfmclass = TekData.TekWaveForm(self.rf_filelist[0], self.dc_filelist[0])
-
-    def mp_assemble_dcmap(self, _threads=4, _plot=False):
+    def mp_assemble_dcmap(self, _threads=4):
         _mpool = Pool(processes=_threads)
-        _outputlist = list(_mpool.map(self.tekwfmclass.mp_compute_dcvoltage, self.dc_filelist))
+        _outputlist = list(_mpool.map(self.tekwfmclass.mp_compute_dcvoltage,
+                           self.dc_filelist))
         _outputdf = pd.DataFrame(_outputlist)
         _mpool.close()
-        _mpool.join()
+        
         self.dc_map = _outputdf
-    
+
         return
-    
-    def compute_dcmask(self, _plot=False):
+
+    def compute_dcmask(self):
         self.dc_mask = (self.dc_map > self.thresholddc)
         return
-    
 
-    def mp_assemble_fftmap(self, _threads=4, _plot=False):
+    def mp_assemble_fftmap(self, _threads=4):
         # Map the rf_filelist through the FFT function and store as rf_map
         _mpool = Pool(processes=_threads)
         _outputlist = list(_mpool.map(
@@ -94,78 +98,20 @@ class TekMap:
         self.rf_map = _outputdf * self.dc_mask
         _mpool.close()
         _mpool.join()
-        
+
         return
-        
+
     def mp_assemble_velocitymap(self, _spacing=12.5E-6):
         self.velocity_map = self.rf_map.multiply(_spacing)
         return
 
-    def plot_fftmap(self, _limits=[80e6, 300e6], _color="gray"):
-        _margin = 1.0
-        _fftplot = plt.figure(figsize=[12, 12])
-        plt.imshow(self.fft_map,aspect=self.waveforms[0].frames_in_file/self.rf_filelist.__len__(), 
-                   cmap=_color)
-        plt.title("FFT Frequency")
-        plt.text(0, 0, "SAW Angle:     $\\alpha$=" + self.metadata["acq_angle"].__str__() + r"$\degree$" +
-                "\nSample ID:     " + self.metadata["sample_desc"])
-        plt.clim(0.080, 0.300)
-        fft_colorbar = plt.colorbar()
-        plt.clim(_limits)
-        fft_colorbar.set_label("Main Frequency [Hz]")
-        plt.xlabel("Record # [200/mm]")
-        plt.ylabel("Row # [10/mm]")
-        x0, x1, y0, y1 = plt.axis()
-        plt.axis((x0-_margin,
-                  x1+_margin,
-                  y0-_margin,
-                  y1+_margin))
-        return
-
-        
     def assemble_velocitymap(self, _spacing=12.5):
         _tempmap = []
-        
+
         for fileidx in range(0, self.waveforms.__len__()):
             self.waveforms[fileidx].compute_velocity(_spacing)
             _tempmap.append(list(self.waveforms[fileidx].velocity_data[0]))
 
         self.velocity_map = pd.DataFrame(_tempmap) * self.dc_mask
-        
+
         return
-    
-    def plot_velocitymap(self, _limits=[2500, 4000], _color="gray"):
-        _margin = 1.0
-        _velplot = plt.figure(figsize=[12, 12])
-        plt.imshow(self.velocity_map, aspect=self.waveforms.frames_in_file/self.rf_filelist.__len__(), cmap=_color)
-        plt.title("Velocity Map")
-        plt.text(0, 0, "SAW Angle:     $\\alpha$=" + self.metadata["acq_angle"].__str__() + r"$\degree$" +
-                "\nSample ID:     " + self.metadata["sample_desc"])
-        plt.clim(0.080, 0.300)
-        fft_colorbar = plt.colorbar()
-        plt.clim(_limits)
-        fft_colorbar.set_label("Velocity [m$^{1}s^{-1}$]")
-        plt.xlabel("Record # [200/mm]")
-        plt.ylabel("Row # [10/mm]")
-        x0, x1, y0, y1 = plt.axis()
-        plt.axis((x0-_margin,
-                  x1+_margin,
-                  y0-_margin,
-                  y1+_margin))
-        return
-
-    def assemble_rf_voltagemap(self):
-        self.rf_voltagemap = []
-        _rec = []
-        for fileidx in range(0, self.waveforms.__len__()):
-            _vt, _ts, _tsc, _tf, _tdf, _td = tekwfm.read_wfm(self.waveforms[fileidx][1].rf_filename)
-            _vtdf = pd.DataFrame(_vt)
-            _record = []
-            for record_idx in range(0, self.waveforms[0][1].frames_in_file):
-                _maxvoltidx = _vtdf[record_idx].argmax()
-                _record.append(_vtdf.loc[_maxvoltidx, record_idx])
-            _rec.append(_record)
-        self.rf_voltagemap = pd.DataFrame(_rec)
-
-        return 
-
--- __main__.py
+++ __main__.py
@@ -7,13 +7,11 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas)
 from PyQt5 import QtWidgets, uic
-
-import MapCollection
+from PyQt5.QtWidgets import QMessageBox
 
 ui_path = '/home/tka/source/visu/'
 ui_file = 'visu2.ui'
 widget_file = 'visu2_worktab.ui'
-
 
 class Ui(QtWidgets.QMainWindow):
 
@@ -32,7 +30,14 @@ class Ui(QtWidgets.QMainWindow):
         self.data_loaded = [False]
         self.dc_processed = [False]
         self.fft_processed = [False]
+        self.view_dc = [False]
+        self.view_fft = [False]
+        self.view_dcm = [False]
+        self.view_vel = [False]
 
+        self.mintab_voltage = [0.0]
+        self.maxtab_voltage = [0.0]
+        self.threshtab_voltage = [0.0]
         # Insert the initial tab into the view.
         self.tab_mainframe.insertTab(0, self.tab_objects[0], "Angle 1")
         self.tab_mainframe.setTabText(1, "Add Angle")
@@ -59,12 +64,13 @@ class Ui(QtWidgets.QMainWindow):
         Ui.addmpl(tab_index, fig): Takes a tab index, and figure, then plots
         the figure into the tab.
         '''
+        self.canvas.append(' ')
         _canvas = FigureCanvas(fig)
         _canvas.minimumWidth = 900
         _canvas.minimumHeight = 900
         self.tab_objects[tab_index].gfx_pane_layout.addWidget(_canvas)
         _canvas.draw()
-        self.canvas.append(_canvas)
+        self.canvas[tab_index] = _canvas
 
         return
 
@@ -105,6 +111,11 @@ class Ui(QtWidgets.QMainWindow):
             self.data_loaded.append(False)
             self.dc_processed.append(False)
             self.fft_processed.append(False)
+            self.view_dc.append(False)
+            self.view_fft.append(False)
+            self.view_dcm.append(False)
+            self.view_vel.append(False)
+            # Tab UI connections
             self.init_angle_tab(_tabcount)
 
         return
@@ -143,6 +154,12 @@ class Ui(QtWidgets.QMainWindow):
         self.tab_objects[index].btn_load.clicked.connect(
                                 functools.partial(self.select_workspace_folder,
                                                   index))
+        self.tab_objects[index].btn_process_dc.clicked.connect(
+                                functools.partial(self.ui_process_dc_voltage,
+                                                  index))
+        self.tab_objects[index].btn_compute_fft.clicked.connect(
+                                functools.partial(self.ui_process_fft,
+                                                  index))
         return
 
     def select_workspace_folder(self, tab_index):
@@ -163,6 +180,105 @@ class Ui(QtWidgets.QMainWindow):
         self.tab_objects[tab_index].btn_load.setText(
             "Change Dataset Directory")
         self.maps.initalize_data(self.workspace_dir[tab_index], tab_index)
+        self.data_loaded[tab_index] = True
+        self.tab_objects[tab_index].btn_process_dc.setEnabled(1)
+        self.tab_objects[tab_index].btn_load.setEnabled(0)
+        return
+
+    def ui_process_dc_voltage(self, tab_index):
+        '''
+            Ui.ui_process_dc_voltage(self, tab_index): Causes the UI to
+            update the DC voltage map and object for this particular tab.
+        '''
+        # if the data hasn't been processed before, process it using
+        # the mp_assemble_dcmap function.
+        if(self.dc_processed[tab_index] is not True):
+            self.maps.scans[tab_index].mp_assemble_dcmap()
+            self.maps.scans[tab_index].compute_dcmask()
+            self.dc_processed[tab_index] = True
+            # Remove current figure from the tab and replace with the
+            # DC voltage view
+            self.remove_mpl(tab_index)
+            _dcfigure = Figure()
+            _dcax = _dcfigure.add_subplot('111')
+            _dcax.imshow(self.maps.scans[tab_index].dc_map,
+                         aspect=self.maps.scans[tab_index].dc_map.shape[1] /
+                         self.maps.scans[tab_index].dc_map.shape[0])
+            self.add_mpl(tab_index, _dcfigure)
+            self.view_dc[tab_index] = True
+            # Update the UI to reflect the new state of the
+            # controls.
+            self.tab_objects[tab_index].btn_process_dc.setText(
+                "Switch to DC Mask View")
+            self.tab_objects[tab_index].btn_compute_fft.setEnabled(1)
+            self.tab_objects[tab_index].txt_sample_name.setEnabled(1)
+            self.tab_objects[tab_index].txt_md_acq_angle.setEnabled(1)
+            self.tab_objects[tab_index].txt_md_startx.setEnabled(1)
+            self.tab_objects[tab_index].txt_md_deltax.setEnabled(1)
+            self.tab_objects[tab_index].txt_md_starty.setEnabled(1)
+            self.tab_objects[tab_index].txt_md_deltay.setEnabled(1)
+
+        else:
+            # If the data was already processed, or logic is toggling
+            # back from the dc mask view just redraw the dc map.
+            if self.view_dc[tab_index] is not True:
+                self.remove_mpl(tab_index)
+                _dcfigure = Figure()
+                _dcaxes = _dcfigure.add_subplot('111')
+                _dcaxes.imshow(self.maps.scans[tab_index].dc_map,
+                               aspect=self.maps.scans[tab_index].dc_map.shape[1] /
+                               self.maps.scans[tab_index].dc_map.shape[0])
+                self.view_dc[tab_index] = True
+                self.view_dcm[tab_index] = False
+                self.view_fft[tab_index] = False
+                self.tab_objects[tab_index].btn_process_dc.setText(
+                    "Switch to DC Mask View")
+                self.add_mpl(tab_index, _dcfigure)
+            else:
+                # Toggle mask view.
+                self.remove_mpl(tab_index)
+                _dcmfigure = Figure()
+                _dcmaxes = _dcmfigure.add_subplot('111')
+                _dcmaxes.imshow(self.maps.scans[tab_index].dc_mask,
+                                aspect=self.maps.scans[tab_index].dc_map.shape[1] /
+                                self.maps.scans[tab_index].dc_map.shape[0])
+                self.view_dc[tab_index] = False
+                self.view_dcm[tab_index] = True
+                self.view_fft[tab_index] = False
+                self.tab_objects[tab_index].btn_process_dc.setText(
+                    "Switch to DC Voltage View")
+                self.add_mpl(tab_index, _dcmfigure)
+
+        return
+
+    def ui_process_fft(self, tab_index):
+        self.tab_objects[tab_index].btn_process_dc.setText(
+                   "Switch to DC Voltage View")
+        if ((self.dc_processed[tab_index] is True) &
+           (self.fft_processed[tab_index] is not True)):
+            # Process FFTs
+            self.maps.scans[tab_index].mp_assemble_fftmap()
+            self.tab_objects[tab_index].le_fft_min.setEnabled(1)
+            self.tab_objects[tab_index].le_fft_max.setEnabled(1)
+            self.fft_processed[tab_index] = True
+            self.tab_objects[tab_index].btn_compute_vel.setEnabled(1)
+            self.tab_objects[tab_index].txt_saw_size.setEnabled(1)
+
+        if ((self.fft_processed[tab_index] is True) &
+           (self.view_fft[tab_index] is False)):
+            # Show FFT on Canvas
+            _fftfig = Figure()
+            _fftax = _fftfig.add_subplot('111')
+            _fftax.imshow(self.maps.scans[tab_index].rf_map,
+                          aspect=self.maps.scans[tab_index].rf_map.shape[1] /
+                          self.maps.scans[tab_index].rf_map.shape[0])
+            self.view_fft[tab_index] = True
+            self.remove_mpl(tab_index)
+            self.add_mpl(tab_index, _fftfig)
+        return
+
+    def ui_process_velocity(self, tab_index):
+
         return
 
 
